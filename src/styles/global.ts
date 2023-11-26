6a365c6f8a3921bfee2a8b7b97123cff1621ae58
sap03110 <wastfg6972@naver.com>
2023-11-26 21:35:46 +0900

    feat: 폰트 적용

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -74,7 +74,21 @@ const globalStyle = css`
   }
 
   * {
-    // font-family: 'KoPubDotumMedium';
+    font-family:
+      Pretendard,
+      -apple-system,
+      BlinkMacSystemFont,
+      system-ui,
+      Roboto,
+      'Helvetica Neue',
+      'Segoe UI',
+      'Apple SD Gothic Neo',
+      'Noto Sans KR',
+      'Malgun Gothic',
+      'Apple Color Emoji',
+      'Segoe UI Emoji',
+      'Segoe UI Symbol',
+      sans-serif;
   }
 `;
 
