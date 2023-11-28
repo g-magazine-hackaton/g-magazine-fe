a3b145f42b5c5af4876b16dd9a1d30ed2c5aeadf
박우림 <woopark@gmarket.com>
2023-11-28 14:35:24 +0900

    chore: 깃허브 페이지 배포 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,42 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import fs from 'fs';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    {
+      name: 'custom-404-plugin',
+      writeBundle() {
+        const distPath = path.resolve(__dirname, 'dist');
+        const custom404Content = `
+          <!DOCTYPE html>
+          <html lang="ko">
+          <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>존재하지 않는 페이지입니다.</title>
+          </head>
+          <body>
+            <h1>404 - 존재하지 않는 페이지입니다.</h1>
+            
+            <p>
+              혹은 "/g-world-fe" 외의 경로를 직접 입력하여 접속을 시도해선 안됩니다. <br>
+              ex: "https://g-world-hackaton.github.io/g-world-fe/my-page" 로 직접 접근.
+            </p>
+
+            <p>아래 버튼을 누르면 Home으로 이동합니다.</p>
+            <a href="/g-world-fe">Go to Home</a>
+          </body>
+          </html>
+        `;
+        fs.writeFileSync(path.join(distPath, '404.html'), custom404Content);
+      },
+    },
+  ],
+  base: "./", // 상대 경로 설정
 });
