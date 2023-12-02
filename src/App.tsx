a2abfe5360b3d8daf813175b474166945e0a3fe7
sap03110 <wastfg6972@naver.com>
2023-12-02 18:33:28 +0900

    feat: 페이지 이동 시마다 scrollTop: 0으로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import styled from '@emotion/styled';
 import { Global } from '@emotion/react';
+import { useEffect } from 'react';
 import Navbar from '@/components/navbar';
 import globalStyle from './styles/global';
 import BottomNavigation from './components/navbar/BottomNavigation';
 
-function App() {
-  const Main = styled.main`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    min-height: 100vh;
-    background-color: #fff;
-  `;
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 100vh;
+  background-color: #fff;
+`;
 
-  const Wrap = styled.div`
-    display: flex;
-    width: 100%;
-    flex-direction: column;
-    margin: 48px 0;
-  `;
+const Wrap = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  margin: 48px 0;
+`;
+
+function App() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }, [pathname]);
 
   return (
     <Main>
